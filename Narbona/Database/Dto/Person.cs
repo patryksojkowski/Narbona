ad40521b1882824777bde51d9c49d149d826313b
Patryk <patryk.sojkowski@hotmail.com>
2023-01-22 19:36:31 +0100

    Added Db Models

---
+++ Narbona/Database/Dto/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Narbona.Database.Dto
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Lastname { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public IList<Email> Emails { get; set; } = new List<Email>();
+
+    }
+}
