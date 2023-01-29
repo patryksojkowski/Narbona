77ea77491cd57a61c95661ed9f7add33ea13f720
Patryk <patryk.sojkowski@hotmail.com>
2023-01-29 23:22:28 +0100

    Added main view (needs some improvements)

--- Narbona/Program.cs
+++ Narbona/Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=People}/{action=Index}");
 
 app.Run();
 
