62b57d18604104a7d252f9e7e6344409c3de0217
Patryk <patryk.sojkowski@hotmail.com>
2023-01-31 21:13:27 +0100

    Added remove action

---
+++ Narbona/wwwroot/js/people.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+
+    $('.table-remove-button').click(e => removeRowHandler(e));
+});
+
+function removeRowHandler(e) {
+    let btn = $(e.target);
+    let personId = (btn.data('person-id'));
+
+    $.ajax({
+        url: "People/Delete?personId=" + personId,
+    }).done(() => {
+        $(`#tr-person-${personId}`).remove();
+    })
+}
